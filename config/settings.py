70d638109a26023f55007f1a290c2123f2dc10b1
MFRSiam <mfrforhad1@gmail.com>
2022-04-15 15:01:06 +0600

    Added Static Files Support

--- config/settings.py
+++ config/settings.py
@@ -118,10 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = str(BASE_DIR.joinpath('static')) # For a ProjectWise Static Files
+# For a ProjectWise Static Files
